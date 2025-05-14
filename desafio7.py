89f71400b2a99e8818f50021fa2032082e727713
Glauber Mathias <glauber.pdlg@gmail.com>
2025-05-14 13:51:14 -0300

    Update desafio7.py

--- desafio7.py
+++ desafio7.py
@@ -1,22 +1,26 @@
-
 import random   
 
 print("Olá, Você está no Banco JWC!")
 
-Titular_da_conta = ("Leonardo Gomes", "Joao Ferreira", "Luis Salomao", "Vitor Azevedo")
-Agencia = ()
-Conta_Corrente = ()
-Saldo = []
-Saque = []
+titular_da_conta = ("Leonardo Gomes", "Joao Ferreira", "Luis Salomao", "Vitor Azevedo")
+agencia = ()
+conta_corrente = ()
+saldo = [0]
+saque = []
+historico = []
 
-Agencia = int(input("Agencia: "))
-Conta_Corrente = int(input("Conta Corrente: "))
-Titular_da_conta = random.choice(Titular_da_conta)
-str(input(Titular_da_conta))
+
+agencia = int(input("Agencia: "))
+conta_corrente = int(input("Conta Corrente: "))
+titular_da_conta = random.choice(titular_da_conta)
+str(input(titular_da_conta))
+
+controle = True
 
 print(50*"-")
 print("Qual operação vai realizar?")
-while True:
+
+while controle == True:
     menu = int(input("\nPara depósito - 1 \nPara saque - 2 \nPara consultar Saldo - 3 \nPara consultar seu extrato - 4 \nDigite aqui: "))
 
     if menu == 1:
@@ -25,11 +29,27 @@ while True:
             deposito = 0
             print("Valor inválido, digite corretamente")
         else:
-            Saldo[0] += deposito
-            print("Informe o valor do seu depósito: R$ {}".format(deposito))
-    elif menu == 2:
-        if Saque <= 0:
-            print("Valor inválido")
-        else:
-            Saldo -= Saque
+            saldo[0] += deposito
+            print("Você depositou: R$ {}".format(deposito))
+            historico.append(deposito)
 
+    elif menu == 2:
+        saque = float(input("Qual o valor do saque?"))
+        if saque <= 0:
+            print("Valor inválido")
+        elif saldo[0] == 0:
+            print("Saldo insuficiente!")
+        elif saque > saldo [0]:
+            print("Saldo insuficiente")
+        else:
+            saldo[0] = saldo[0] - saque
+            print("Saque de R$ {} realizado".format(saque))
+            historico.append(-saque)
+    elif menu == 3:
+        print("Saldo atualizado: R$ {}".format(saldo))
+    elif menu == 4:
+        print("Olhe seu extrato: {}".format(historico))
+    else:
+        controle == False
+        print("Volte sempre Sr(a). {}".format(titular_da_conta))
+        break
